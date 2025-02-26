33d8e3ea41e8571d5bfc0a2647fa4fc5acc75eb7
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-02-26 11:21:54 +0200

    fixed error with secret key

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -16,13 +16,14 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = os.path.join(BASE_DIR, ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
+load_dotenv(ENV_PATH)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
